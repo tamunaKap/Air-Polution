f99137fd3e7e28179cff5b67df42fd3399ee4426
unknown <tamuna@saba.com.ge>
2015-05-12 21:51:46 +0400

    air

---
+++ scripts/DraggingElements.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $('.drag').draggable({
+        opacity: 0.2,
+        revert: true
+    });
+
+    $('.drop').droppable({
+        hoverClass: 'drop1',
+        over: function () {
+            $(this).text('Please drop in anytime.');
+        },
+        out: function () {
+            $(this).text('Goodbye');
+        },
+        drop: function (event, ui) {
+            var that = $(this);
+            that.text('Thanks!');
+            $(ui.draggable).hide(1000);
+            setTimeout(function () {
+                $(ui.draggable).show(1000);
+                that.text('Dropbox');
+            }, 5000);
+        }
+    });
+})
+
---
+++ scripts/Elements.js
@@ -0,0 +1,64 @@
+﻿$(document).ready(function () {
+    $("#arrow").click(function () { 
+        $('body').animate({ scrollTop: $('#PollutionReasons').offset().top }, 'slow');
+    });
+    BoxAnimations()
+    $(window).scroll(function () { BoxAnimations() });
+    function BoxAnimations() {
+        $("#box1").boxLoader({
+            direction: "x",
+            position: "50%",
+            effect: "fadeIn",
+            duration: "1s",
+            windowarea: "50%"
+        });
+
+        $("#box2").boxLoader({
+            direction: "x",
+            position: "-50%",
+            effect: "fadeIn",
+            duration: "3s",
+            windowarea: "50%"
+        });
+
+        $("#box3").boxLoader({
+            direction: "y",
+            position: "100%",
+            effect: "none",
+            duration: "2s",
+            windowarea: "50%"
+        });
+
+        $("#box4").boxLoader({
+            direction: "y",
+            position: "100%",
+            effect: "fadeIn",
+            duration: "1s",
+            windowarea: "50%"
+        });
+
+        $("#box5").boxLoader({
+            direction: "x",
+            position: "100%",
+            effect: "fadeIn",
+            duration: "3s",
+            windowarea: "50%"
+        });
+        $("#box6").boxLoader({
+            direction: "y",
+            position: "100%",
+            effect: "none",
+            duration: "2s",
+            windowarea: "50%"
+        });
+        $("#box7").boxLoader({
+            direction: "none",
+            position: "none",
+            effect: "fadeIn",
+            duration: "1s",
+            windowarea: "50%"
+        });
+
+    }
+
+})
